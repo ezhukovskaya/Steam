6905df2b4de48ca3e754d4b02a0f16d6cfa8cb9d
e.zhukovskaya <Roc86kipi>
2019-11-20 21:02:14 +0300

    almost done

--- src/test/java/tests/TC2.java
+++ src/test/java/tests/TC2.java
@@ -1,20 +1,30 @@
 package tests;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pageObjects.forms.Action;
+import pageObjects.forms.Genres;
+import pageObjects.forms.Indie;
 import pageObjects.pages.AnyCategoryGamesPage;
 
 public class TC2 extends BaseTest {
     private final int HIGH_DISCOUNT = 1;
     private final String BROWSING_ACTION = "BROWSING_ACTION";
+    private final int LOW_DISCOUNT = 0;
+    private final String BROWSING_INDIE = "BROWSING_INDIE";
     private AnyCategoryGamesPage anyCategoryGamesPage = new AnyCategoryGamesPage();
 
-    @Test
-    public void highestDiscountCalculationCheck() {
+    @DataProvider
+    public Object[][] getData() {
+        return new Object[][]{{new Action(), BROWSING_ACTION, HIGH_DISCOUNT}, {new Indie(), BROWSING_INDIE, LOW_DISCOUNT}};
+    }
+
+    @Test(dataProvider = "getData")
+    public void highestDiscountCalculationCheck(Genres genres,String genre, int discount) {
         Assert.assertTrue(mainPage.isHomePageDisplayed(), "The page is not opened");
         log.info("Go to Action page");
-        mainPage.goToMainMenu().goToGamesCategory().goToGenre(new Action());
+        mainPage.goToMainMenu().goToGamesCategory().goToGenre(genres);
         Assert.assertTrue(anyCategoryGamesPage.genrePageIsDisplayed(BROWSING_ACTION), "Action page is not opened");
         log.info("Find the game with the biggest discount");
         anyCategoryGamesPage.topSellingClick();
